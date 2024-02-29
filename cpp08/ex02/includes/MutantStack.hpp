07fc7389da48a8964f1faeb51e56a7085a38ea5c
Celeste <naterrie@student.42lyon.fr>
2024-02-29 14:01:32 +0100

    clear of .hpp

--- cpp08/ex02/includes/MutantStack.hpp
+++ cpp08/ex02/includes/MutantStack.hpp
@@ -8,15 +8,23 @@ template <typename T>
 class MutantStack : public std::stack<T>
 {
 	public:
-		MutantStack() : std::stack<T>() {}
-		MutantStack(MutantStack const &other) : std::stack<T>(other) {}
+		MutantStack() : std::stack<T>()
+		{
+		}
+
+		MutantStack(MutantStack const &other) : std::stack<T>(other)
+		{
+		}
+
 		MutantStack &operator=(MutantStack const &other)
 		{
 			if (this != &other)
 				std::stack<T>::operator=(other);
 			return *this;
 		}
-		~MutantStack() {}
+		~MutantStack()
+		{
+		}
 
 		typedef typename std::stack<T>::container_type::iterator iterator;
 
