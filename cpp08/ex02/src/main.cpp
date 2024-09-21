a098a8c81850993877327516b18804ea3364ca34
Celeste <naterrie@student.42lyon.fr>
2024-09-21 20:32:57 +0200

    edit test for cpp08

--- cpp08/ex02/src/main.cpp
+++ cpp08/ex02/src/main.cpp
@@ -1,4 +1,5 @@
 #include "MutantStack.hpp"
+#include <list>
 
 int main()
 {
@@ -39,3 +40,58 @@ int main()
 			std::cout << *it << ", ";
 	}
 }
+
+// Test for std::list
+
+// #include <iostream>
+// #include <list>
+// #include <stack>
+
+// int main()
+// {
+//     {
+//         std::list<int> mlist;
+//         mlist.push_back(5);
+//         mlist.push_back(17);
+//         std::cout << mlist.back() << std::endl;  // Equivalent to mstack.top()
+//         mlist.pop_back();                        // Equivalent to mstack.pop()
+//         std::cout << mlist.size() << std::endl;
+
+//         mlist.push_back(3);
+//         mlist.push_back(5);
+//         mlist.push_back(737);
+//         // [...]
+//         mlist.push_back(0);
+
+//         std::list<int>::iterator it = mlist.begin();
+//         std::list<int>::iterator ite = mlist.end();
+//         ++it;
+//         --it;
+//         while (it != ite)
+//         {
+//             std::cout << *it << std::endl;
+//             ++it;
+//         }
+
+//         // In C++98, you can't directly initialize a std::stack with a std::list
+//         std::stack<int> s;
+//         for (std::list<int>::iterator it = mlist.begin(); it != mlist.end(); ++it)
+//         {
+//             s.push(*it);  // Manually push list elements into the stack
+//         }
+//     }
+//     std::cout << "----------------" << std::endl;
+//     {
+//         std::list<int> mlist;
+//         for (int i = 0; i < 10; i++)
+//             mlist.push_back(i);
+//         std::cout << mlist.back() << std::endl;  // Equivalent to mstack.top()
+//         std::cout << mlist.size() << std::endl;
+//         mlist.pop_back();                        // Equivalent to mstack.pop()
+//         std::cout << mlist.back() << std::endl;
+
+//         for (std::list<int>::iterator it = mlist.begin(); it != mlist.end(); ++it)
+//             std::cout << *it << ", ";
+//         std::cout << std::endl;
+//     }
+// }
